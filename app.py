4e46e4cedf6a2944046c4675e4dd11d7ecc9c982
ndjndj <teq.ndj@gmail.com>
2021-05-16 23:18:37 +0900

    change dir templates

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def hello_world():
 
 @app.route('/u-rotation')
 def u_rotation():
-    return render_template('./static/html/rotation.html')
+    return render_template('rotation.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
