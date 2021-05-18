1deb1a3e4a32e6de5f1b21c7e2083976de8a5c33
ndjndj <teq.ndj@gmail.com>
2021-05-18 18:40:10 +0900

    models.py

---
+++ models/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from models.database import base
+from datetime import datetime
+
+class UName(Base):
+    __tablename__ = 'u-name'
+    u_id = Column(Integer, primary_key=True)
+    u_name = Column(String(128))
+    created_at = Column(DateTime, default=datetime.now())
+    updated_at = Column(DateTime, default=datetime.now())
+
+    def __init__(self, u_name=None, created_at=None, updated_at=None):
+        
