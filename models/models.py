851ebe6dea49715b630c877fa6d3ffa6b46fb509
ndjndj <teq.ndj@gmail.com>
2021-05-18 18:40:20 +0900

    models.py

--- models/models.py
+++ models/models.py
@@ -8,6 +8,4 @@ class UName(Base):
     u_name = Column(String(128))
     created_at = Column(DateTime, default=datetime.now())
     updated_at = Column(DateTime, default=datetime.now())
-
-    def __init__(self, u_name=None, created_at=None, updated_at=None):
-        
+\
