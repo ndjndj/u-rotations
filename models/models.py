ad5f3a28336ec2a51feb845edaaad8ba482d39ea
ndjndj <teq.ndj@gmail.com>
2021-05-21 01:04:02 +0900

    URaceSchedule

--- models/models.py
+++ models/models.py
@@ -17,3 +17,19 @@ class UName(Base):
 
     def __repr__(self):
         return '<U_NAME %r>' % (self.u_name)
+
+class URaceSchedule(Base):
+    __tablename__ = 'u-race-schedule'
+    u_id = Column(Integer, primary_key=True)
+    race_id = Column(Integer)
+    created_at = Column(DateTime, default=datetime.now())
+    updated_at = Column(DateTime, default=datetime.now())
+
+    def __init__(self, u_id=None, race_id=None, created_at=None, updated_at=None):
+        self.u_id = u_id
+        self.race_id = race_id
+        self.created_at = created_at
+        self.updated_at = updated_at
+
+    def __repr__(self):
+        return '<U_id %r>' % (self.u_id)
