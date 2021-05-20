3f82516878aef448b74fe68d091192eadb3aa062
ndjndj <teq.ndj@gmail.com>
2021-05-21 01:06:27 +0900

    RaceCalendar

--- models/models.py
+++ models/models.py
@@ -33,3 +33,21 @@ class URaceSchedule(Base):
 
     def __repr__(self):
         return '<U_id %r>' % (self.u_id)
+
+class RaceCalendar(Base):
+    __tablename__ = 'race-calendar'
+    race_id = Column(Integer, primary_key=True)
+    calendar_id = Column(Integer)
+    race_name = Column(String(128))
+    created_at = Column(DateTime, default=datetime.now())
+    updated_at = Column(DateTime, default=datetime.now())
+
+    def __init__(self, race_id=None, calendar_id=None, race_name=None, created_at=None, updated_at=None):
+        self.race_id = race_id
+        self.calendar_id = calendar_id
+        self.race_name = race_name
+        self.created_at = created_at
+        self.updated_at = updated_at
+
+    def __repr__(self):
+        return '<race_name %r>' % (self.race_name)
