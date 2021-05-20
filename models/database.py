e5a093646b57b7762ac514fc7ef1211694048bff
ndjndj <teq.ndj@gmail.com>
2021-05-20 23:38:32 +0900

    db info

--- models/database.py
+++ models/database.py
@@ -2,3 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
+
+database_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'u_rotations.db')
+engine = create_engine('sqlite:///' + database_file, covert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
