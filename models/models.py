20f019edaa8fa7f257ed675d44bfbfb38ff2bc77
ndjndj <teq.ndj@gmail.com>
2021-05-23 00:44:17 +0900

    models.py

--- models/models.py
+++ models/models.py
@@ -5,20 +5,20 @@ from datetime import datetime
 class UName(Base):
     __tablename__ = 'u-name'
     u_id = Column(Integer, primary_key=True)
+    u_farm_name = Column(String(128))
     u_name = Column(String(128))
-    u_name_ex = Column(String(128))
     created_at = Column(DateTime, default=datetime.now())
     updated_at = Column(DateTime, default=datetime.now())
 
-    def __init__(self, u_id=None, u_name=None, u_name_ex=None, created_at=None, updated_at=None):
+    def __init__(self, u_id=None, u_farm_name=None, u_name=None, created_at=None, updated_at=None):
         self.u_id = u_id
+        self.u_farm_name = u_farm_name
         self.u_name = u_name
-        self.u_name_ex = u_name_ex
         self.created_at = created_at
         self.updated_at = updated_at
 
     def __repr__(self):
-        return '<U_NAME_EX %r>' % (self.u_name_ex)
+        return '<U_FARM_NAME %r>' % (self.u_farm_name)
 
 class URaceSchedule(Base):
     __tablename__ = 'u-race-schedule'
