6d643d6d3736dec2afffac6229f9d6cc356731f3
ndjndj <teq.ndj@gmail.com>
2021-05-24 02:46:14 +0900

    app u-name

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
+from logging import NullHandler
 from flask import Flask, render_template, request
+from models.models import UName
 
 app = Flask(__name__)
 
@@ -11,6 +13,13 @@ def hello_world():
 @app.route('/u-rotation')
 def u_rotation():
     return render_template('rotation.html')
+
+@app.route('/u-names')
+def u_names():
+    u_names_data = UName.query.all()
+    return render_template("u-name.html", u_names=u_names_data)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
